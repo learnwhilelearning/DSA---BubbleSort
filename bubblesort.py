2b9109b79618d375fde239e5cb9e15b90ff66825
learnwhilelearning <109062125+learnwhilelearning@users.noreply.github.com>
2024-05-16 18:03:55 +0530

    Create bubblesort.py

---
+++ bubblesort.py
@@ -0,0 +1,16 @@
+def bubble_sort(arr):
+  
+  end = len(arr)
+
+  for i in range(end-1):
+    for j in range(end -1):
+      if (arr[j]>arr[j+1]):
+        tmp = arr[j]
+        arr[j]=arr[j+1]
+        arr[j+1]=tmp
+  
+if __name__=='__main__':
+  elements = [11,5,9,1,6,14,12,11,13]
+
+  bubble_sort(elements)
+  print(elements)
